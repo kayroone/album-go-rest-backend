2160bb2db6fa9db5eb9fa739409c1e08388c5fc0
Wiegmann <jan.wiegmann@c02fq296md6r.fritz.box>
2024-03-01 13:58:00 +0100

    Upgraded deps

---
+++ entity/album.go
@@ -0,0 +1,17 @@
+package entity
+
+import (
+	"fmt"
+)
+
+type Album struct {
+	ID     int    `json:"id" db:"id"`
+	Title  string `json:"title" db:"title"`
+	Artist string `json:"artist" db:"artist"`
+	Price  int    `json:"price" db:"price"`
+}
+
+func (a Album) printAlbum() {
+
+	fmt.Printf("%d, %s, %s, %d", a.ID, a.Title, a.Artist, a.Price)
+}
